f6af31b52aa8c24ba4b066bfcfd5038e3c362ef7
aqron <winn403@hotmail.com>
2017-11-15 03:31:52 +0000

    is

---
+++ rps.js
@@ -0,0 +1,16 @@
+
+
+function app() {
+    // listen for click on play button
+}
+
+function play() {
+    // call this when they clicked play
+    // unhide the 3 buttons
+    // add event listeners for the three buttons here.
+    
+    // when they click on any of the buttons you should probably 
+}
+
+// this will run the app
+app();
